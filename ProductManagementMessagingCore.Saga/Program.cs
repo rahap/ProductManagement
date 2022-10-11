7d22496598a855dc48f2628c682f4a6eec284fba
SELCUK TOPCU <SELCUK.TOPCU@Lcwaikiki.com>
2022-10-11 15:08:57 +0300

    saga  object added

---
+++ ProductManagementMessagingCore.Saga/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductManagementMessagingCore.Saga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
